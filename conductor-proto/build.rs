07599ff1a80b267726132584e158f4bef29da0b5
brendangraham14 <brendanigraham@gmail.com>
2025-06-20 11:05:38 -0700

    feat(proto): introduce new conductor-proto crate generating gRPC code

---
+++ conductor-proto/build.rs
@@ -0,0 +1,12 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../proto");
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .compile_protos(
+            &["../proto/streaming.proto", "../proto/remote_workspace.proto"],
+            &["../proto"],
+        )?;
+    Ok(())
+}
---
+++ conductor-proto/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod agent {
+    tonic::include_proto!("conductor.agent.v1");
+}
+pub mod remote_workspace {
+    tonic::include_proto!("conductor.remote_workspace.v1");
+}
+
