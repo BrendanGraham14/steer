6eeb9c3e99fc0f8f780bbe12e8df5adaf50f3959
Brendan Graham <brendanigraham@gmail.com>
2025-07-31 00:24:21 -0700

    chore: vendored protoc

--- crates/steer-proto/build.rs
+++ crates/steer-proto/build.rs
@@ -1,6 +1,9 @@
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Use vendored protoc to avoid requiring users to install it
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+
     // Get the manifest directory (where Cargo.toml is located)
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR")?;
     let manifest_path = PathBuf::from(manifest_dir);
@@ -14,27 +17,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let remote_workspace_proto = proto_dir.join("steer/remote_workspace/v1/remote_workspace.proto");
     let common_proto = proto_dir.join("steer/common/v1/common.proto");
 
-    // Compile common proto first since others depend on it
+    let mut config = prost_build::Config::new();
+    config.protoc_executable(protoc.clone());
     tonic_build::configure()
         .build_server(false)
         .build_client(false)
         .type_attribute(".", "#[allow(clippy::large_enum_variant)]")
-        .compile_protos(
+        .compile_protos_with_config(
+            config,
             &[common_proto.to_str().unwrap()],
             &[proto_dir.to_str().unwrap()],
         )?;
 
-    // Compile agent and remote_workspace protos
+    let mut config = prost_build::Config::new();
+    config.protoc_executable(protoc.clone());
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
         .type_attribute(".", "#[allow(clippy::large_enum_variant)]")
-        .compile_protos(
+        .compile_protos_with_config(
+            config,
             &[
                 agent_proto.to_str().unwrap(),
                 remote_workspace_proto.to_str().unwrap(),
             ],
             &[proto_dir.to_str().unwrap()],
         )?;
+
     Ok(())
 }
