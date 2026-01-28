3c542d2beab0f65c4cf7176086e23006ea09194c
Brendan Graham <brendanigraham@gmail.com>
2026-01-28 15:16:39 -0800

    chore: clippy configs

--- crates/steer-proto/src/lib.rs
+++ crates/steer-proto/src/lib.rs
@@ -1,17 +1,47 @@
 pub mod common {
     pub mod v1 {
+        #![allow(
+            clippy::all,
+            dead_code,
+            non_camel_case_types,
+            non_snake_case,
+            non_upper_case_globals,
+            unfulfilled_lint_expectations,
+            unused_imports,
+            unused_variables
+        )]
         tonic::include_proto!("steer.common.v1");
     }
 }
 
 pub mod agent {
     pub mod v1 {
+        #![allow(
+            clippy::all,
+            dead_code,
+            non_camel_case_types,
+            non_snake_case,
+            non_upper_case_globals,
+            unfulfilled_lint_expectations,
+            unused_imports,
+            unused_variables
+        )]
         tonic::include_proto!("steer.agent.v1");
     }
 }
 
 pub mod remote_workspace {
     pub mod v1 {
+        #![allow(
+            clippy::all,
+            dead_code,
+            non_camel_case_types,
+            non_snake_case,
+            non_upper_case_globals,
+            unfulfilled_lint_expectations,
+            unused_imports,
+            unused_variables
+        )]
         tonic::include_proto!("steer.remote_workspace.v1");
     }
 }
